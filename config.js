55fac1124f44d99ea353d36677abe0da9933267e
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-12-30 16:05:02 +0100

    pingAPItest

---
+++ config.js
@@ -0,0 +1,3 @@
+const BASE_URL = "https://restful-booker.herokuapp.com";
+
+module.exports = { BASE_URL };
--- tests/ping.spec.js
+++ tests/ping.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
+const { BASE_URL  } = require('../config');
 
 test('API alive',async({request})=>{
-    const response = await request.get("https://restful-booker.herokuapp.com/ping");
+const response = await request.get(`${BASE_URL}/ping`);
     expect(response.status()).toBe(201)
 });

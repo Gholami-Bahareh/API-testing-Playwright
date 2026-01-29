49768e3ec2209db20905a97765260c25f4266401
Gholami-Bahareh <baharehgholamii86@gmail.com>
2026-01-29 22:51:23 +0100

    improvements

---
+++ api/booking.api.js
@@ -0,0 +1,21 @@
+class BookingApi {
+
+  async createBooking(request, body, token) {
+    throw new Error('Not implemented yet');
+  }
+
+  async getBookingById(request, id, token) {
+    throw new Error('Not implemented yet');
+  }
+
+  async updateBooking(request, id, body, token) {
+    throw new Error('Not implemented yet');
+  }
+
+  async deleteBooking(request, id, token) {
+    throw new Error('Not implemented yet');
+  }
+
+}
+
+module.exports = BookingApi;
